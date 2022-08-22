9b48328b3f3d14f670a99b7351cab17685bf7350
Joshua Mulla <joshuamulla@Joshuas-MacBook-Pro.local>
2022-08-21 21:25:03 -0700

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,33 @@
 module.exports = {
   siteMetadata: {
     title: `Photo Blog`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    // {
+    //   resolve: 'gatsby-source-contentful',
+    //   options: {
+    //     "accessToken": "",
+    //     "spaceId": ""
+    //   }
+    // },
+    // "gatsby-plugin-styled-components",
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "images"
-  }]
+  ],
 };
